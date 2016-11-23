106f722c93912622fad4060367869e5c45dd80ca
Jiri Peinlich <jiri.peinlich@gmail.com>
2016-11-23 20:47:22 +0000

    adding spring

---
+++ src/main/java/cz/peinlich/EnableJsonApplication.java
@@ -0,0 +1,12 @@
+package cz.peinlich;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnableJsonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnableJsonApplication.class, args);
+	}
+}
